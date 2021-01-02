0a223089c5d2f1bafb0ee9d5abb9884b86ddc757
ssywh97 <ssywh97@gmail.com>
2021-01-03 02:36:02 +0900

    자바스크립트 공부

--- javaScript/array.js
+++ javaScript/array.js
@@ -37,3 +37,72 @@ console.log(arr2);
     console.log(result); // // [3,4,5]
     console.log(array); // [1,2,3,4,5]
 }
+class Student{
+    constructor(name, age, enrolled, score){
+        this.name = name;
+        this.age = age;
+        this.enrolled = enrolled;
+        this.score = score;
+    }
+}
+
+const students = [
+    new Student('A', 29, true, 45),
+    new Student('B', 28, false, 80),
+    new Student('C', 30, true, 90),
+    new Student('D', 40, false, 66),
+    new Student('E', 18, true, 88),
+];
+//Q5. find a student with the score 90
+{
+    const result = students.find((student) => student.score === 90);
+    console.log(result);
+}
+
+//Q6. make an array of enrolled students
+{
+    const result = students.filter((student) => student.enrolled === true);
+    console.log(result);
+
+}
+
+//Q7. make an array containing only the students scores
+//result should be : [45, 80, 90, 66, 88]
+{
+    const result = students.map((student) => student.score);
+    console.log(result);
+}
+
+//Q8. check if there is a student with the score lower than 50
+{
+    // const result = students.filter((student) => student.score < 50); 이럴땐 그 값이 반환되고
+    const result = students.filter((student) => student.score < 50);//이거는 있는지 없는지, 곧 true or false 반환
+   //students.every는 모두 만족해야 true
+    console.log(result);
+}
+
+//Q9. compute students' average score
+{
+    const result = students.reduce((prev, curr) => prev + curr.score,0); //학생의 score을 누적해서 더해감
+     console.log(result/students.length);
+}
+
+//Q10. make a string containing all the scores
+// result should be : '45,8,0,90,66,88'
+{
+    const result = students
+    .map((student) => student.score)
+    .filter((score) => score >= 50)
+    .join();
+    console.log(result);
+}
+
+//BONUS! do Q10 sorted in ascending order
+//result should be: '45, 66, 80, 88, 90'
+{
+    const result = students
+    .map((student) => student.score)
+    .sort((a, b) => a - b)
+    .join();
+    console.log(result);
+}
